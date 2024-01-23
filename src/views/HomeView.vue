c83f7ad2232a003c91c713db764191b742b216ea
victorinojohnmark <victorinojohnmark@gmail.com>
2024-01-23 16:28:25 +0800

    Boilerplate done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-gray-50 w-full border-b border-gray-100 mb-5">
-    <div class="w-full border-b border-gray-100">
+    <div class="w-full md:border-b md:border-gray-100">
       <Menubar :model="items" class="rounded-none container mx-auto border-0">
         <template #start class="">
           <div class="flex ml-3 mr-5 gap-x-3 items-center">
@@ -26,7 +26,7 @@
       </Menubar>
     </div>
 
-    <MegaMenu :model="megaMenuItems" class="container mx-auto border-0 rounded-none" />
+    <MegaMenu :model="megaMenuItems" class="hidden md:block container mx-auto border-0 rounded-none" />
   </header>
   <main class="px-3">
     <section id="banner" class="container mx-auto rounded-md mb-5">
@@ -53,7 +53,138 @@
           </div>
         </div>
       </div>
-    </section>  
+    </section> 
+    
+    <section id="categories" class="container mx-auto mb-5">
+        <div class="flex flex-wrap items-center justify-center text-center w-full md:w-2/3 xl:w-full mx-auto">
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-car text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Cars</p>
+                </div>
+            </a>
+
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-home text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Properties For Sale</p>
+                </div>
+            </a>
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-building text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Properties for Rent</p>
+                </div>
+            </a>
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-box text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Products</p>
+                </div>
+            </a>
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-car text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Trending</p>
+                </div>
+            </a>
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-verified text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Services</p>
+                </div>
+            </a>
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-tablet text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Gadgets</p>
+                </div>
+            </a>
+            <a href="#" class="group flex justify-center items-center w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/12 p-3 aspect-square rounded-md hover:shadow-md shadow">
+                <div class="text-center">
+                    <i class="pi pi-shopping-bag text-red-600 text-2xl mb-3"></i>
+                    <p class="text-gray-950 text-sm font-semibold group-hover:underline group-hover:decoration-red-600">Clothes</p>
+                </div>
+            </a>
+
+        </div>
+    </section>
+
+    <section id="marketingBanners" class="container mx-auto mb-5">
+        <div class="flex flex-col md:flex-row gap-x-3">
+            <img src="/img/property-marketing.jpg" class="w-full md:w-1/2 rounded-md mb-3" alt="">
+            <img src="/img/car-marketing.jpg" class="w-full md:w-1/2 rounded-md mb-3" alt="">
+        </div>
+    </section>
+
+    <section id="items" class="container mx-auto mb-5">
+        <div id="popularItems">
+            <div class="flex flex-row justify-between">
+                <strong class="text-gray-800 text-xl">Items for Sale</strong>
+                <i class="pi pi-arrow-right text-gray-700"></i>
+            </div>
+
+            <div class="flex flex-wrap flex-row overflow-hidden pb-5 mb-5">
+                <div class="item-card w-1/2 md:w-1/4 lg:w-1/5 p-3 px-2">
+                    <a href="#" class="">
+                        <div class="rounded p-3 shadow-md hover:shadow-lg">
+                            <img src="/img/kitchen.jpg" alt="" class="rounded-md aspect-video object-cover object-center">
+                            <strong class="text-red-600 leading-8">AFN 135,000</strong>
+                            <p class="text-gray-900 text-sm font-semibold">Studio • 1 Bath</p>
+                            <p class="text-sm">Kabul</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="item-card w-1/2 md:w-1/4 lg:w-1/5 p-3 px-2">
+                    <a href="#" class="">
+                        <div class="rounded p-3 shadow-md hover:shadow-lg">
+                            <img src="/img/bike.jpeg" alt="" class="rounded-md aspect-video object-cover object-center">
+                            <strong class="text-red-600 leading-8">AFN 5,000</strong>
+                            <p class="text-gray-900 text-sm font-semibold">JAVA Hybrid Bike</p>
+                            <p class="text-sm">Herat</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="item-card w-1/2 md:w-1/4 lg:w-1/5 p-3 px-2">
+                    <a href="#" class="">
+                        <div class="rounded p-3 shadow-md hover:shadow-lg">
+                            <img src="/img/phone.jpeg" alt="" class="rounded-md aspect-video object-cover object-center">
+                            <strong class="text-red-600 leading-8">AFN 1,000</strong>
+                            <p class="text-gray-900 text-sm font-semibold">Samsung S22 Ultra</p>
+                            <p class="text-sm">Herat</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="item-card w-1/2 md:w-1/4 lg:w-1/5 p-3 px-2">
+                    <a href="#" class="">
+                        <div class="rounded p-3 shadow-md hover:shadow-lg">
+                            <img src="/img/pickup-truck.jpeg" alt="" class="rounded-md aspect-video object-cover object-center">
+                            <strong class="text-red-600 leading-8">AFN 250,000</strong>
+                            <p class="text-gray-900 text-sm font-semibold">Toyota Hilux 2023</p>
+                            <p class="text-sm">Kandahar</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="item-card w-1/2 md:w-1/4 lg:w-1/5 p-3 px-2">
+                    <a href="#" class="">
+                        <div class="rounded p-3 shadow-md hover:shadow-lg">
+                            <img src="/img/lamp.jpeg" alt="" class="rounded-md aspect-video object-cover object-center">
+                            <strong class="text-red-600 leading-8">AFN 4000</strong>
+                            <p class="text-gray-900 text-sm font-semibold">Lamp Shade</p>
+                            <p class="text-sm">Kandahar</p>
+                        </div>
+                    </a>
+                </div>
+                
+                
+                
+            </div>
+        </div>
+    </section>
       
   </main>
 </template>
@@ -86,7 +217,7 @@ const items = ref([
     },
     {
         label: 'Favorites',
-        icon: 'pi pi-heart text-gray-700',
+        icon: 'pi pi-heart text-red-600',
         // badge: 3
     },
     {
